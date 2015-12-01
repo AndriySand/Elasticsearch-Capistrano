b1c00c3e2bbc66a45e1f71971a241471aeb3877b
Andriy Samokhin <asam@softserveinc.com>
2015-12-01 04:55:33 -0800

    Refactored Record class

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,12 @@
 class Record < ActiveRecord::Base
+
+    def formatted_value
+      begin
+        special_class = (klass.capitalize + 'Formatter').constantize
+        special_class.format(value)
+      rescue
+        value
+      end
+    end
+
 end
