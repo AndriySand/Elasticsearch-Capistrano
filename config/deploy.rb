c3032b8f34d734aa64d808206606fed2e6c638c2
Andriy Samokhin <asam@softserveinc.com>
2015-12-08 04:52:57 -0800

    Added correct path where to deploy application

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:AndriySand/Elasticsearch-Capistrano.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-  set :deploy_to, '/home/andriy/projects'
+  set :deploy_to, '/home/andriy/projects/elasticsearch-capistrano'
 
 # Default value for :scm is :git
 # set :scm, :git
